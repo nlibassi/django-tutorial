ca22e2f640250d5e83d5ab7a58f7ddbaf8aa2044
Nick <nlibassi@gmail.com>
2019-01-18 11:17:45 +0300

    sample view without template

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,35 @@
 #polls/urls.py (URLconf)
 
+# using django.conf.urls.url for now but may be deprecated soon
 from django.conf.urls import url
+# django.urls.path is only available in Django 2.0
+#from django.urls import path
 
 from . import views
 
+# understand difference between django.conf.urls.url and django.urls.path
+
+# should a regex be given as first argument of url?
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # ex: /polls/
+    url('', views.index, name='index'),
+    # ex: /polls/5/
+    url('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+"""
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+"""
