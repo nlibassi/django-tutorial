6f82d6d4a54f8982af3eb88a8ec18ecece9b75e2
Nick <nlibassi@gmail.com>
2019-02-14 13:18:22 +0300

    further edits on admin site, added project templates, finished tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -4,13 +4,24 @@ from .models import Choice, Question
 
 # Register your models here.
 
+# had first tried admin.StackedInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 # sample modification below adds fieldsets
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                      {'fields': ['question_text']}),
-        ('Date information',   {'fields': ['pub_date']}),
+        ('Date information',   {'fields': ['pub_date'], 'classes': ['collapse']}),
         ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# removed after making Choice inline - adding choices on the question page
+#admin.site.register(Choice)
